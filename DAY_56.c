168fd4db3f91752323edcdffac7534d0ab3c6146
praneethsai434 <varada.28521@stu.upes.ac.in>
2026-03-28 20:40:17 +0530

    Create DAY_56.c

---
+++ DAY_56.c
@@ -0,0 +1,87 @@
+// 56) Check whether a given binary tree is symmetric around its center.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node *left;
+    struct Node *right;
+} Node;
+
+Node* createNode(int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+int isMirror(Node* t1, Node* t2) {
+    if (t1 == NULL && t2 == NULL)
+        return 1;
+
+    if (t1 == NULL || t2 == NULL)
+        return 0;
+
+    if (t1->data != t2->data)
+        return 0;
+
+    return isMirror(t1->left, t2->right) && isMirror(t1->right, t2->left);
+}
+
+int isSymmetric(Node* root) {
+    if (root == NULL)
+        return 1;
+    return isMirror(root->left, root->right);
+}
+
+Node* buildTree(int arr[], int n) {
+    if (n == 0 || arr[0] == -1)
+        return NULL;
+
+    Node** nodes = (Node**)malloc(n * sizeof(Node*));
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == -1)
+            nodes[i] = NULL;
+        else
+            nodes[i] = createNode(arr[i]);
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (nodes[i] != NULL) {
+            int leftIndex = 2 * i + 1;
+            int rightIndex = 2 * i + 2;
+
+            if (leftIndex < n)
+                nodes[i]->left = nodes[leftIndex];
+            if (rightIndex < n)
+                nodes[i]->right = nodes[rightIndex];
+        }
+    }
+
+    Node* root = nodes[0];
+    free(nodes);
+    return root;
+}
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    int arr[N];
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    Node* root = buildTree(arr, N);
+
+    if (isSymmetric(root))
+        printf("YES\n");
+    else
+        printf("NO\n");
+
+    return 0;
+}
+
