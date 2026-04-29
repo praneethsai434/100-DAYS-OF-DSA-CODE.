74c92f310158e2a17de712a71d8e336716bec6c6
praneethsai434 <varada.28521@stu.upes.ac.in>
2026-04-29 19:41:03 +0530

    Create DAY_86.c

---
+++ DAY_86.c
@@ -0,0 +1,26 @@
+//86) Find the integer square root of a given non-negative integer using Binary Search. The integer square root of a number is defined as the greatest integer whose square is less than or equal to the given number.
+
+
+#include <stdio.h>
+
+int main() {
+    long long n;
+    scanf("%lld", &n);
+
+    long long low = 0, high = n, ans = 0;
+
+    while (low <= high) {
+        long long mid = low + (high - low) / 2;
+
+        if (mid * mid <= n) {
+            ans = mid;          // mid can be answer
+            low = mid + 1;      // search for greater value
+        } else {
+            high = mid - 1;     // mid is too large
+        }
+    }
+
+    printf("%lld", ans);
+
+    return 0;
+}
