cd0a8bed03616b73817156c0e0aafb8d28e70ba9
praneethsai434 <varada.28521@stu.upes.ac.in>
2026-05-01 22:39:01 +0530

    Create DAY_100.c

---
+++ DAY_100.c
@@ -0,0 +1,57 @@
+// 100)  Problem: For each element, count how many smaller elements appear on right side.
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Car {
+    int position;
+    int speed;
+};
+
+int compareCars(const void *a, const void *b) {
+    struct Car *c1 = (struct Car *)a;
+    struct Car *c2 = (struct Car *)b;
+
+    return c2->position - c1->position;  // descending order
+}
+
+int carFleet(int target, struct Car cars[], int n) {
+    qsort(cars, n, sizeof(struct Car), compareCars);
+
+    int fleets = 0;
+    double previousTime = 0.0;
+
+    for (int i = 0; i < n; i++) {
+        double currentTime = (double)(target - cars[i].position) / cars[i].speed;
+
+        if (currentTime > previousTime) {
+            fleets++;
+            previousTime = currentTime;
+        }
+    }
+
+    return fleets;
+}
+
+int main() {
+    int n, target;
+
+    scanf("%d %d", &n, &target);
+
+    struct Car cars[n];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &cars[i].position);
+    }
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &cars[i].speed);
+    }
+
+    int answer = carFleet(target, cars, n);
+
+    printf("%d\n", answer);
+
+    return 0;
+}
